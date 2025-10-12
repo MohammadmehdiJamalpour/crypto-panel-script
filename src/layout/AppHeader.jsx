a2d3e3cd2a0dc0b73604d504579f3a64d87b464d
mohammadmehdijamalpour <mohammadmehdi.jamalpour28@gmail.com>
2025-10-12 20:07:16 +0330

    header background is updated

--- src/layout/AppHeader.jsx
+++ src/layout/AppHeader.jsx
@@ -6,19 +6,30 @@ const avatar = "/profile-defualt.jpg";
 
 export default function AppHeader() {
   return (
-    <header
+       <header
       className="
-        relative
-        top-0 z-40 w-full min-h-56 border-b border-white/10
-        bg-[url('/header-bg.svg')] bg-no-repeat bg-center
-        bg-[length:70%] md:bg-[length:65%] lg:bg-[length:60%]
+        relative top-0 z-40 w-full min-h-56 border-b border-white/10
+        overflow-hidden rounded-3xl
       "
     >
+      {/* BG layer: ONLY this scales horizontally */}
+      <div
+        className="
+          pointer-events-none absolute inset-0 -z-10
+          bg-[url('/header-bg.svg')] bg-no-repeat bg-center
+          bg-contain 
+          origin-center scale-x-105 md:scale-x-125 lg:scale-x-140 lg:scale-y-110 xl:scale-y-100 xl:scale-x-150 
+          transition-transform
+        "
+        aria-hidden
+      />
+
+      {/* Your header content */}
       <ProfileBadge
         name="Bizhan Bahrami"
         subtitle="Sales Materials"
         avatarSrc={avatar}
-        className="absolute top-[20%] left-[10%]"
+        className="absolute top-[10%] left-[10%] md:top-[15%]"
       />
     </header>
   );
