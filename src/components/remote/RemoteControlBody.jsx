a56f3af77ead1aa2750e11c5989e12327a9bfce3
mohammadmehdijamalpour <mohammadmehdi.jamalpour28@gmail.com>
2025-10-22 22:29:06 +0330

    remote control base components added

---
+++ src/components/remote/RemoteControlBody.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RemoteControlBody() {
+  return (
+    <div>RemoteControlBody</div>
+  )
+}
+
+export default RemoteControlBody
---
+++ src/components/remote/RemoteControlLimitation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RemoteControlLimitation() {
+  return (
+    <div>RemoteControlLimitation</div>
+  )
+}
+
+export default RemoteControlLimitation
